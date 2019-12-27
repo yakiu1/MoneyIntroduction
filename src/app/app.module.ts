e349d267018c6c3e2d7568c0c48c0d3f687ffb12
Gon <yakiu100@gmail.com>
2019-12-27 13:27:25 +0800

    Gon 2019/12/27 - CSS框架 Bootstrap & Materaial
    1. 除了安裝還有去Custom Materaial  主題

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MoneyDeatilComponent } from './money-deatil/money-deatil.component';
+import { MatSidenavModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule, MatListModule, MatMenuModule } from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,15 @@ import { MoneyDeatilComponent } from './money-deatil/money-deatil.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
